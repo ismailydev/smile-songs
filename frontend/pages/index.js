8eb767b59ef6c7b8e594366b90ca5d6542026439
Ismail <ismail.yimer.h@gmail.com>
2023-02-21 18:37:38 +0300

    setup tailwindcss

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -16,7 +16,9 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-                <h1>Welcome to smile-songs</h1>
+                <h1 className="text-3xl font-bold underline">
+                    Welcome to smile-songs
+                </h1>
             </main>
         </>
     );
