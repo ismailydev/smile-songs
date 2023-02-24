6f76636aed8ce253e8ce2106347ce66e11bd6526
Ismail <ismail.yimer.h@gmail.com>
2023-02-24 12:30:07 +0300

    fixed components styles

--- frontend/components/Song.js
+++ frontend/components/Song.js
@@ -7,16 +7,9 @@ import { deleteSong } from "@/features/songsSlice";
 export default function Song({ song }) {
     const { _id: id, title, artist, album, genre } = song;
 
-    const dispatch = useDispatch();
-
-    const handleDeleteSong = (id) => {
-        dispatch(deleteSong(id));
-        console.log("deleted song " + id);
-    };
-
     return (
         <div className="relative">
-            <Link key={id} href={`/songs/${id}`}>
+            <Link href={`/songs/${id}`}>
                 <div className="relative bg-black p-5 flex items-center justify-between rounded-3xl transition-colors duration-300 ease-in-out hover:bg-slate-300 hover:text-black cursor-pointer">
                     <div className="flex gap-4 items-center">
                         <div className="w-10 h-10 flex items-center justify-center">
@@ -37,14 +30,15 @@ export default function Song({ song }) {
                     <p className="text-xs">{album}</p>
                 </div>
             </Link>
-            <div className="absolute right-0 -bottom-3 bg-slate-800 w-20 py-2 rounded-xl cursor-pointer">
-                <p className="text-center text-xs text-slate-200">{genre}</p>
-            </div>
-            <div
-                onClick={() => handleDeleteSong(id)}
-                className="absolute left-0 -bottom-3 bg-red-800 w-20 py-2 rounded-xl cursor-default"
-            >
-                <p className="text-center text-xs text-slate-200">delete</p>
+            <div className="absolute right-0 -bottom-3 flex gap-2 transition-opacity duration-300 ease-in-out">
+                <Link
+                    href={`/genres/${genre}`}
+                    className="bg-slate-800 w-20 py-2 rounded-xl cursor-pointer hover:bg-slate-900 transition-colors duration-300 ease-in-out"
+                >
+                    <p className="text-center text-[10px] text-slate-200 lowercase">
+                        {genre}
+                    </p>
+                </Link>
             </div>
         </div>
     );
