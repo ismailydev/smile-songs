782d3e15cf62e4205379c557d130088f2a6cdb95
Ismail <ismail.yimer.h@gmail.com>
2023-02-28 14:39:38 +0300

    song controllers - getSong

--- backend/controllers/song.js
+++ backend/controllers/song.js
@@ -11,4 +11,19 @@ module.exports = {
             return res.status(400).json({ error: error.message });
         }
     },
+    getSong: async (req, res) => {
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ error: "No such song." });
+        }
+
+        const song = await Song.findById(id);
+
+        if (!song) {
+            return res.status(404).json({ error: "No such song." });
+        }
+
+        res.status(200).json(song);
+    },
 };
