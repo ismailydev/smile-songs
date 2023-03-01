f5153b4bea024a1842a73b625b492b7013bbbac9
Ismail <ismail.yimer.h@gmail.com>
2023-03-01 15:14:25 +0300

    json parser and message

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,15 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/songs", require("./routes/song"));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send(
+        "<h1>Welcome to <b>smile-songs-api</b></h1><p>Checkout /api/songs</p>"
+    );
 });
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
