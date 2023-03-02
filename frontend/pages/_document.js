1fd23ca02cb13dca2aa0c8af3f16f1ff9c089309
Ismail <ismail.yimer.h@gmail.com>
2023-03-02 17:50:31 +0300

    first commit

---
+++ frontend/pages/_document.js
@@ -0,0 +1,28 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="true"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Lora:wght@500;700&family=Poppins:wght@400;500;700&display=swap"
+                    rel="stylesheet"
+                />
+                <link
+                    href="https://fonts.cdnfonts.com/css/gilroy-bold"
+                    rel="stylesheet"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
