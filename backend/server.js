68c8ef413a14f7d69982522209f40eb08bcd9134
Ismail <ismail.yimer.h@gmail.com>
2023-03-03 20:32:29 +0300

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const connectDB = require("./config/db");
@@ -7,6 +9,7 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
