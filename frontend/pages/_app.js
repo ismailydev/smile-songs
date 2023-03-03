1d888cfb3a3c2c1a5a9068e9cc1d43ff1cff5c67
Ismail <ismail.yimer.h@gmail.com>
2023-03-03 20:36:48 +0300

    wrap whole app with provider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Layout from "./layout";
+import { Provider } from "react-redux";
+import store from "../features/store";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <Provider store={store}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </Provider>
+    );
 }
