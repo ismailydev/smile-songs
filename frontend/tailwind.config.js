e62f3d4c90caf82bd2ae1a6d42d642d0899cf39e
Ismail <ismail.yimer.h@gmail.com>
2023-03-07 11:25:44 +0300

    added next themes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,12 @@ module.exports = {
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
+    darkMode: "class",
     theme: {
         extend: {
+            colors: {
+                dark: "#0D0D0D",
+            },
             fontFamily: {
                 lora: ["Lora", "serif"],
                 poppins: ["Poppins", "sans-serif"],
